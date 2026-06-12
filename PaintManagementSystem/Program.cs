1e1eb2fc7a89e8059f89f206b8ece9dc9fe954f6
Alison Sun <alisonsun@Alisons-MacBook-Pro.local>
2026-06-12 22:37:12 +1000

    finished program file

--- PaintManagementSystem/Program.cs
+++ PaintManagementSystem/Program.cs
@@ -1,12 +1,22 @@
 ﻿using PaintManagementSystem.Enums;
 using PaintManagementSystem.Models;
 
-// Testing Classes
-PaintSpecification paintSpecification = new PaintSpecification("Pink", 2);
-Console.WriteLine(paintSpecification.DisplaySpecification());
+// create many paintproduct products
+PaintSpecification paintSpecification1 = new PaintSpecification("Pink", 2);
+PaintSpecification paintSpecification2 = new PaintSpecification("White", 4);
+PaintSpecification paintSpecification3 = new PaintSpecification("Blue", 8);
 
-PaintProduct paintProduct = new PaintProduct("Love", PaintType.Glossy, paintSpecification, 30);
-Console.WriteLine(paintProduct.DisplayInfo());
+PaintProduct paintProduct1 = new PaintProduct("Pink Paint", PaintType.Matte, paintSpecification1, 100m);
+PaintProduct paintProduct2 = new PaintProduct("White Paint", PaintType.Glossy, paintSpecification2, 80m);
+PaintProduct paintProduct3 = new PaintProduct("Blue Paint", PaintType.BaseCoat, paintSpecification3, 60m);
 
-Order order = new Order(paintProduct, 2);
-Console.WriteLine(order.DisplayOrder());
+// display all the Info for paintProducts
+Console.WriteLine(paintProduct1.DisplayInfo());
+Console.WriteLine(paintProduct2.DisplayInfo());
+Console.WriteLine(paintProduct3.DisplayInfo());
+
+// create an order
+Order order1 = new Order(paintProduct1, 6);
+
+// display the Info for order1
+Console.WriteLine(order1.DisplayOrder());
