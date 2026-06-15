14356dc27093437bea34b58cb1361a23220e477e
Alison Sun <alisonsun@Alisons-MacBook-Pro.local>
2026-06-15 13:20:28 +1000

    added payment class

---
+++ PaintManagementSystem/Models/Payment.cs
@@ -0,0 +1,22 @@
+namespace PaintManagementSystem.Models;
+using PaintManagementSystem.Enums;
+
+public class Payment
+{
+    private static int _nextPaymentId = 1;
+    public int PaymentId {get;}
+    public PaymentStatus Status {get; private set;}
+    public decimal PaymentAmount {get; private set;}
+    public PaymentMethod Method {get; private set;}
+    public Order Order {get;}
+
+    public Payment(PaymentStatus paymentStatus, PaymentMethod paymentMethod, Order order)
+    {
+        PaymentId = _nextPaymentId ++;
+        Status = paymentStatus;
+        Method = paymentMethod;
+        Order = order;
+        PaymentAmount = Order.TotalPrice;
+    }
+}
+
