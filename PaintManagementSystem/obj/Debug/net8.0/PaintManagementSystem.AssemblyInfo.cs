b382ac70b8e59cd370fd159713d27972bb0f4fd0
Alison Sun <alisonsun@Alisons-MacBook-Pro.local>
2026-06-15 14:59:17 +1000

    assembly update

--- PaintManagementSystem/obj/Debug/net8.0/PaintManagementSystem.AssemblyInfo.cs
+++ PaintManagementSystem/obj/Debug/net8.0/PaintManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaintManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd0f22ce783cce2df0d119d174d94210177036e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30aea53515310893759c05c51ffad2fab2c9ac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaintManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaintManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
